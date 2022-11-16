e802a90d3cfd145ae229ba73978669746148edcb
Alexandra <alexandrareis25@gmail.com>
2022-11-16 00:37:08 -0300

    Post Put e Delete da model de Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ import Home from "./paginas/home/Home";
 import Login from "./paginas/login/Login";
 import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
-
 import "./App.css";
 import ListaTema from "./components/temas/listatema/ListaTema";
 import ListaPostagem from "./components/postagens/listapostagem/ListaPostagem";
+import CadastroPost from "./components/postagens/cadastroPost/CadastroPost";
+import CadastroTema from "./components/temas/cadastroTema/CadastroTema";
+import DeletarPostagem from "./components/postagens/deletarPostagem/DeletarPostagem";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
+
 
 function App() {
   return (
@@ -17,14 +21,23 @@ function App() {
       <Navbar />
       <div style={{ minHeight: "100vh" }}>
         <Routes>
-                
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/cadastro" element={<CadastroUsuario />} />
           <Route path="/temas" element={<ListaTema />} />
           <Route path="/postagens" element={<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
 
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
         </Routes>
       </div>
       <Footer />
--- src/components/temas/listatema/ListaTema.tsx
+++ src/components/temas/listatema/ListaTema.tsx
@@ -1,46 +1,95 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Box, Card, CardActions, CardContent, Button, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import {Box, Button, Card,CardActions,CardContent,Typography,} from '@mui/material';
+import Tema from '../../../models/Tema';
+import { busca } from '../../../services/Service';
+import useLocalStorage from 'react-use-localstorage';
 import './ListaTema.css';
+
 
 function ListaTema() {
 
-  return (
-    <>
-      <Box m={2} >
-        <Card variant="outlined">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Tema
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Minha descrição
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5} >
-
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                    atualizar
-                  </Button>
-                </Box>
-              </Link>
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" size='small' color="secondary">
-                    deletar
-                  </Button>
-                </Box>
-              </Link>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
-    </>
-  );
-}
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
 
 
-export default ListaTema;
+
+
+let history = useNavigate()
+  useEffect(() => {
+    if(token === '') {
+      alert('Você precisa estar logado pra ficar aqui')
+      history('/login')
+    }
+  }, [token])
+ 
+ 
+    
+    let navigate = useNavigate();
+  
+    useEffect(()=>{
+      if(token == ''){
+        alert("Você precisa estar logado")
+        navigate("/login")
+      }
+    }, [token])
+  
+  
+    async function getTema(){
+      await busca("/tema", setTemas, {
+        headers: {
+          'Authorization': token
+        }
+      })
+    }
+  
+  
+    useEffect(()=>{
+      getTema()
+    }, [temas.length])
+  
+    return (
+      <>
+      {
+        temas.map(tema =>(
+        <Box m={2} >
+          <Card variant="outlined">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Tema
+              </Typography>
+              <Typography variant="h5" component="h2">
+               {tema.descricao}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5} >
+  
+                <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                  <Box mx={1}>
+                    <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                  <Box mx={1}>
+                    <Button variant="contained" size='small' color="secondary">
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </Card>
+        </Box>
+        ))
+        }
+      </>
+    );
+  }
+  
+
+  export default ListaTema;
+  
+
